fd634d1e746594f727585c02d5b8f151e0de8c1b
anashali01 <saiyedanashali@gmail.com>
2026-01-30 10:41:06 +0530

    animation start

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,2 +1,13 @@
 let tl = gsap.timeline();
 
+tl.from("header .menu .header-menu  ,header span , header .btn-talk",{
+    y : -100,
+    opacity : 0,
+    duration : 0.45,
+    stagger : 0.2
+})
+tl.from("header .btn-talk a",{
+    y : 40,
+    opacity : 0,
+    duration : 0.45
+})
